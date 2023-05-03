40b2f4908f80b528b05de05735d14b0d8a5e97b3
DustinWidner <108431103+MrTacoTastic@users.noreply.github.com>
2023-05-03 02:23:11 -0400

    Remove nonsense

--- C++/Transmog/Transmog.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct ItemTemplate;
-
-bool IsTransmogItem(const ItemTemplate *it);
-bool HasTransmog(unsigned int AcctId, unsigned int ItemId);
-void AddTransmog(unsigned int AcctId, unsigned int ItemId);
